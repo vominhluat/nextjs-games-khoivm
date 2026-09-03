e84279c201a8b16313a7427422822154cc9a00cc
vominhluat <vominhluat@gmail.com>
2026-09-03 15:25:01 +0700

    init app

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
               width={16}
               height={14}
             />
-            Deploy Now
+            LuatVM
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
@@ -60,7 +60,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            LuatVM
           </a>
         </div>
       </main>
